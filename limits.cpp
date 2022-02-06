6b6fc601a14febe9a27ffdd5f6a16538adb22981
hyr <yuruihong02@outlook.com>
2022-02-06 22:41:49 +0800

    test

---
+++ limits.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+    > File Name: limits.c
+    > Author: HongYurui
+    > Mail: yuruihong02@outlook.com 
+    > Created Time: Sat 05 Feb 2022 10:53:00 PM CST
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+#include<cmath>
+using namespace std;
+int main() {
+    cout  << isinf(10 / (double)0);
+}
